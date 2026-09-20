432528581a8d23e76339f62575e47925c844882d
Wijesinghe Mudiyanselage Sewwandi Shashikala Wijesinghe <sewwandiwijesinghe68@gmail.com>
2026-09-20 17:37:09 +0530

    r

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,15 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas සම්බන්ධ කිරීම
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// ඔබේ API routes මෙහි ලියන්න
+  
 app.get('/api/equipment', async (req, res) => {
   res.json({ message: "Success" });
 });
 
-// Vercel සඳහා export කිරීම (app.listen පාවිච්චි නොකරන්න)
 module.exports = app;
